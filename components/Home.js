ba3c1a056b4a0ebdf258b52f999f8c1f38c9df87
bemin.shaker <beminshaker1@outlook.com>
2022-12-17 22:56:09 -0500

    improved file structure

--- components/Home.js
+++ components/Home.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Home() {
   return (
     <View style={styles.container}>
+      {/* Joined Communities */}
+      {/* Nearby Communities */}
+      {/* Bottom Navbar */}
       <Text>Home Screen</Text>
     </View>
   );
