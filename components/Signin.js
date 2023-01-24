21a9e2e491508f71be507329b421f8c18f53ab58
TheShicksinator <54967556+TheShicksinator@users.noreply.github.com>
2023-01-24 18:44:02 -0500

    Add a back button component Fixes #8

--- components/Signin.js
+++ components/Signin.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import {
-  StyleSheet,
-  View,
-  Dimensions,
-  Text,
-  Pressable,
-  ScrollView,
+    StyleSheet,
+    View,
+    Dimensions,
+    Text,
+    Pressable,
+    ScrollView,
 } from "react-native";
 import { TextInput, Snackbar, Button } from "react-native-paper";
 import { useFonts, Montserrat_700Bold } from "@expo-google-fonts/montserrat";
@@ -16,164 +16,175 @@ import { logInWithEmail } from "../backend/firebase";
  */
 
 export default function Signin({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [visible, setVisible] = React.useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [visible, setVisible] = React.useState(false);
 
-  const onDismissSnackBar = () => setVisible(false);
+    const onDismissSnackBar = () => setVisible(false);
 
-  let [fontsLoaded] = useFonts({
-    Montserrat_700Bold,
-  });
+    let [fontsLoaded] = useFonts({
+        Montserrat_700Bold,
+    });
 
-  if (!fontsLoaded) {
-    return null;
-  }
+    if (!fontsLoaded) {
+        return null;
+    }
 
-  return (
-    <>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.headerText}>Login</Text>
-          <Text style={styles.headerTextSmall}>
-            Welcome back, you’ve been missed!
-          </Text>
-          <View style={styles.curve}>
-            <View style={styles.signupForm}>
-              <TextInput
-                style={styles.textInput}
-                activeUnderlineColor="#C88D36"
-                underlineColor="#DADADA"
-                textColor="#DADADA"
-                label="Email"
-                left={<TextInput.Icon icon="email-outline" />}
-                onChangeText={(email) => setEmail(email)}
-              />
-              <TextInput
-                style={styles.textInput}
-                activeUnderlineColor="#C88D36"
-                underlineColor="#DADADA"
-                textColor="#DADADA"
-                label="Password"
-                secureTextEntry
-                left={<TextInput.Icon icon="lock-outline" />}
-                onChangeText={(text) => setPassword(text)}
-              />
+    return (
+        <>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.headerText}>Login</Text>
+                    <Text style={styles.headerTextSmall}>
+                        Welcome back, you’ve been missed!
+                    </Text>
+                    <View style={styles.curve}>
+                        <View style={styles.signupForm}>
+                            <TextInput
+                                style={styles.textInput}
+                                activeUnderlineColor="#C88D36"
+                                underlineColor="#DADADA"
+                                textColor="#DADADA"
+                                label="Email"
+                                left={<TextInput.Icon icon="email-outline" />}
+                                onChangeText={(email) => setEmail(email)}
+                            />
+                            <TextInput
+                                style={styles.textInput}
+                                activeUnderlineColor="#C88D36"
+                                underlineColor="#DADADA"
+                                textColor="#DADADA"
+                                label="Password"
+                                secureTextEntry
+                                left={<TextInput.Icon icon="lock-outline" />}
+                                onChangeText={(text) => setPassword(text)}
+                            />
 
-              <Pressable
-                style={styles.signupButton}
-                onPress={async () => {
-                  let result = await logInWithEmail(email, password);
-                  if (result === "success") {
-                    navigation.navigate("Home");
-                  } else {
-                    {
-                      setVisible(!visible);
-                    }
-                  }
-                }}
-              >
-                <Text style={styles.signupButtonText}>Login</Text>
-              </Pressable>
-              <Pressable onPress={() => navigation.navigate("Signup")}>
-                <Text style={styles.loginButton}>
-                  Don't have an account?{" "}
-                  <Text style={styles.loginButtonHighlight}>Sign up</Text>
-                </Text>
-              </Pressable>
-              <Snackbar
-                style={styles.snackbar}
-                visible={visible}
-                onDismiss={onDismissSnackBar}
-                action={{
-                  label: "Dismiss",
-                  labelStyle: { color: "#C88D36" },
-                  onPress: () => {
-                    // Do something
-                  },
-                }}
-              >
-                <Text style={styles.errorMsg}>Invalid Login Credentials</Text>
-              </Snackbar>
-            </View>
-          </View>
-        </View>
-      </ScrollView>
-    </>
-  );
+                            <Pressable
+                                style={styles.signupButton}
+                                onPress={async () => {
+                                    let result = await logInWithEmail(
+                                        email,
+                                        password
+                                    );
+                                    if (result === "success") {
+                                        navigation.navigate("Home");
+                                    } else {
+                                        {
+                                            setVisible(!visible);
+                                        }
+                                    }
+                                }}
+                            >
+                                <Text style={styles.signupButtonText}>
+                                    Login
+                                </Text>
+                            </Pressable>
+                            <Pressable
+                                onPress={() => navigation.navigate("Signup")}
+                            >
+                                <Text style={styles.loginButton}>
+                                    Don't have an account?{" "}
+                                    <Text style={styles.loginButtonHighlight}>
+                                        Sign up
+                                    </Text>
+                                </Text>
+                            </Pressable>
+                            <Snackbar
+                                style={styles.snackbar}
+                                visible={visible}
+                                onDismiss={onDismissSnackBar}
+                                action={{
+                                    label: "Dismiss",
+                                    labelStyle: { color: "#C88D36" },
+                                    onPress: () => {
+                                        // Do something
+                                    },
+                                }}
+                            >
+                                <Text style={styles.errorMsg}>
+                                    Invalid Login Credentials
+                                </Text>
+                            </Snackbar>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#000000",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 40,
-  },
-  textInput: {
-    width: Dimensions.get("screen").width * 0.8,
-    marginBottom: 10,
-    color: "white",
-    backgroundColor: "transparent",
-    opacity: 1,
-  },
-  signupButton: {
-    backgroundColor: "#C88D36",
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 30,
-    paddingRight: 30,
-    width: Dimensions.get("screen").width * 0.8,
-    borderRadius: 15,
-    marginBottom: 15,
-    marginTop: 30,
-  },
-  signupButtonText: {
-    color: "white",
-    fontSize: 17,
-    textAlign: "center",
-  },
-  loginButton: {
-    color: "white",
-    fontSize: 17,
-    marginTop: 7,
-  },
-  loginButtonHighlight: {
-    color: "#C88D36",
-  },
-  curve: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#212121",
-    width: Dimensions.get("screen").width * 1,
-    borderTopStartRadius: 45,
-    borderTopEndRadius: 45,
-    overflow: "hidden",
-    marginTop: 80,
-    paddingTop: 10,
-    paddingBottom: 60,
-  },
-  headerText: {
-    fontFamily: "Montserrat_700Bold",
-    fontSize: 20,
-    color: "#FFFFFF",
-    marginBottom: 10,
-    marginTop: 80,
-  },
-  headerTextSmall: {
-    fontSize: 14,
-    color: "#FFFFFF",
-  },
-  signupForm: {
-    alignItems: "center",
-    paddingTop: 30,
-    height: Dimensions.get("screen").height * 0.7,
-  },
-  errorMsg: {
-    color: "white",
-  },
-  snackbar: {
-    marginBottom: 40,
-  },
+    container: {
+        backgroundColor: "#000000",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingBottom: 40,
+    },
+    textInput: {
+        width: Dimensions.get("screen").width * 0.8,
+        marginBottom: 10,
+        color: "white",
+        backgroundColor: "transparent",
+        opacity: 1,
+    },
+    signupButton: {
+        backgroundColor: "#C88D36",
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 30,
+        paddingRight: 30,
+        width: Dimensions.get("screen").width * 0.8,
+        borderRadius: 15,
+        marginBottom: 15,
+        marginTop: 30,
+    },
+    signupButtonText: {
+        color: "white",
+        fontSize: 17,
+        textAlign: "center",
+    },
+    loginButton: {
+        color: "white",
+        fontSize: 17,
+        marginTop: 7,
+    },
+    loginButtonHighlight: {
+        color: "#C88D36",
+    },
+    curve: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#212121",
+        width: Dimensions.get("screen").width * 1,
+        borderTopStartRadius: 45,
+        borderTopEndRadius: 45,
+        overflow: "hidden",
+        marginTop: 80,
+        paddingTop: 10,
+        paddingBottom: 60,
+    },
+    headerText: {
+        fontFamily: "Montserrat_700Bold",
+        fontSize: 20,
+        color: "#FFFFFF",
+        marginBottom: 10,
+        marginTop: 80,
+    },
+    headerTextSmall: {
+        fontSize: 14,
+        color: "#FFFFFF",
+    },
+    signupForm: {
+        alignItems: "center",
+        paddingTop: 30,
+        height: Dimensions.get("screen").height * 0.7,
+    },
+    errorMsg: {
+        color: "white",
+    },
+    snackbar: {
+        marginBottom: 40,
+    },
 });
--- components/User/joinCommunity.js
+++ components/User/joinCommunity.js
@@ -1,126 +1,128 @@
 import * as React from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Pressable,
-  Image,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    Pressable,
+    Image,
 } from "react-native";
 
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
 import {
-  useFonts,
-  Montserrat_400Regular,
-  Montserrat_700Bold,
+    useFonts,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
 } from "@expo-google-fonts/montserrat";
 import { joinCommunity, getEmail, getUser } from "../../backend/firebase";
 
 export default function JoinCommunity({ route, navigation }) {
-  let [fontsLoaded] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_700Bold,
-  });
+    let [fontsLoaded] = useFonts({
+        Montserrat_400Regular,
+        Montserrat_700Bold,
+    });
 
-  if (!fontsLoaded) {
-    return null;
-  }
+    if (!fontsLoaded) {
+        return null;
+    }
 
-  return (
-    <View style={styles.container}>
-      <Pressable onPress={() => navigation.navigate("Home")}>
-        <Text style={{ color: "white" }}>Back</Text>
-      </Pressable>
-      <View style={styles.header}>
-        <Text style={styles.lightText}>This is</Text>
-        <Text style={styles.boldText}>{route.params.name}</Text>
-        <Text style={styles.count}>
-          {route.params.count} Members {" • "} {route.params.type}
-        </Text>
-      </View>
-      <Image
-        style={styles.image}
-        source={require("../../assets/city.jpg")}
-      ></Image>
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={() => navigation.goBack()}>
+                <Text style={{ color: "white" }}>Back</Text>
+            </Pressable>
+            <View style={styles.header}>
+                <Text style={styles.lightText}>This is</Text>
+                <Text style={styles.boldText}>{route.params.name}</Text>
+                <Text style={styles.count}>
+                    {route.params.count} Members {" • "} {route.params.type}
+                </Text>
+            </View>
+            <Image
+                style={styles.image}
+                source={require("../../assets/city.jpg")}
+            ></Image>
 
-      <Pressable
-        style={styles.loginButton}
-        onPress={async () => {
-          let usersEmail = await getEmail();
-          let usersId = await getUser(usersEmail);
-          let submit = await joinCommunity(
-            usersEmail,
-            route.params.id,
-            route.params.name,
-            usersId[0].id
-          );
-          if (submit == "success") {
-            navigation.navigate("CommunityFeed", {
-              id: route.params.id,
-              name: route.params.name,
-            });
-          }
-        }}
-      >
-        <Text style={styles.loginButtonText}>Join Community</Text>
-      </Pressable>
-    </View>
-  );
+            <Pressable
+                style={styles.loginButton}
+                onPress={async () => {
+                    let usersEmail = await getEmail();
+                    let usersId = await getUser(usersEmail);
+                    let submit = await joinCommunity(
+                        usersEmail,
+                        route.params.id,
+                        route.params.name,
+                        usersId[0].id
+                    );
+                    if (submit == "success") {
+                        navigation.navigate("CommunityFeed", {
+                            id: route.params.id,
+                            name: route.params.name,
+                        });
+                    }
+                }}
+            >
+                <Text style={styles.loginButtonText}>Join Community</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#000000",
-    paddingTop: 150,
-    padding: 20,
-    height: Dimensions.get("screen").height * 1,
-    alignItems: "center",
-  },
+    container: {
+        backgroundColor: "#000000",
+        paddingTop: 150,
+        padding: 20,
+        height: Dimensions.get("screen").height * 1,
+        alignItems: "center",
+    },
 
-  lightText: {
-    fontFamily: "Montserrat_400Regular",
-    color: "#DADADA",
-    fontSize: 20,
-    marginTop: 20,
-  },
-  count: {
-    fontFamily: "Montserrat_400Regular",
-    color: "#DADADA",
-    fontSize: 15,
-    marginTop: 15,
-  },
-  boldText: {
-    fontSize: 25,
-    marginTop: 10,
-    color: "white",
-    fontFamily: "Montserrat_700Bold",
-    textAlign: "center",
-  },
-  header: {
-    textAlign: "center",
-    alignItems: "center",
-  },
-  loginButton: {
-    backgroundColor: "#C88D36",
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 30,
-    paddingRight: 30,
-    width: Dimensions.get("screen").width * 0.8,
-    borderRadius: 15,
-    marginBottom: 15,
-    marginTop: 65,
-  },
-  loginButtonText: {
-    color: "white",
-    fontSize: 17,
-    textAlign: "center",
-  },
-  image: {
-    width: Dimensions.get("screen").width * 0.8,
-    height: Dimensions.get("screen").height * 0.25,
+    lightText: {
+        fontFamily: "Montserrat_400Regular",
+        color: "#DADADA",
+        fontSize: 20,
+        marginTop: 20,
+    },
+    count: {
+        fontFamily: "Montserrat_400Regular",
+        color: "#DADADA",
+        fontSize: 15,
+        marginTop: 15,
+    },
+    boldText: {
+        fontSize: 25,
+        marginTop: 10,
+        color: "white",
+        fontFamily: "Montserrat_700Bold",
+        textAlign: "center",
+    },
+    header: {
+        textAlign: "center",
+        alignItems: "center",
+    },
+    loginButton: {
+        backgroundColor: "#C88D36",
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 30,
+        paddingRight: 30,
+        width: Dimensions.get("screen").width * 0.8,
+        borderRadius: 15,
+        marginBottom: 15,
+        marginTop: 65,
+    },
+    loginButtonText: {
+        color: "white",
+        fontSize: 17,
+        textAlign: "center",
+    },
+    image: {
+        width: Dimensions.get("screen").width * 0.8,
+        height: Dimensions.get("screen").height * 0.25,
 
-    borderRadius: 400,
-    opacity: 1,
-    marginTop: 30,
-  },
+        borderRadius: 400,
+        opacity: 1,
+        marginTop: 30,
+    },
 });
