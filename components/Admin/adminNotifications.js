0f2284667b144e8487f4fa1e36a1d78d45c41808
bemin.shaker <beminshaker1@outlook.com>
2023-02-23 14:30:23 -0500

    more adjustments to dark mode toggle

--- components/Admin/adminNotifications.js
+++ components/Admin/adminNotifications.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { StyleSheet, View, Pressable, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-// import Icon from "react-native-vector-icons/Ionicons";
+import { useTheme } from "../../theme/ThemeProvider";
 import { List } from "react-native-paper";
 import Back from "../Back";
 import {
@@ -13,7 +13,7 @@ import {
 
 export default function AdminNotifications() {
   const navigation = useNavigation();
-
+  const { colors } = useTheme();
   let [fontsLoaded] = useFonts({
     Montserrat_700Bold,
     Montserrat_600SemiBold,
@@ -21,19 +21,28 @@ export default function AdminNotifications() {
   });
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
       <Back />
-      <Text style={styles.header}>Notifications</Text>
+      <Text style={[styles.header, { color: colors.text }]}>
+        Notification Feed
+      </Text>
 
       <List.Section>
         <List.Item
           title="A user reported a post in your community."
           titleNumberOfLines={2}
           key={1}
-          titleStyle={styles.header2}
-          style={styles.listItem}
+          titleStyle={[styles.header2, { color: colors.text }]}
+          style={[
+            styles.listItem,
+            { borderBottomColor: colors.borderBottomColor },
+          ]}
           left={() => (
-            <List.Icon color={"#323232"} style={styles.iconLeft} icon="alert" />
+            <List.Icon
+              color={colors.text}
+              style={[styles.iconLeft, { backgroundColor: colors.fabBgColor }]}
+              icon="alert"
+            />
           )}
           right={() => <List.Icon color={"#ADADAD"} icon="dots-horizontal" />}
         />
@@ -41,10 +50,17 @@ export default function AdminNotifications() {
           title="A user reported a post in your community."
           titleNumberOfLines={2}
           key={2}
-          titleStyle={styles.header2}
-          style={styles.listItem}
+          titleStyle={[styles.header2, { color: colors.text }]}
+          style={[
+            styles.listItem,
+            { borderBottomColor: colors.borderBottomColor },
+          ]}
           left={() => (
-            <List.Icon color={"#323232"} style={styles.iconLeft} icon="alert" />
+            <List.Icon
+              color={colors.text}
+              style={[styles.iconLeft, { backgroundColor: colors.fabBgColor }]}
+              icon="alert"
+            />
           )}
           right={() => <List.Icon color={"#ADADAD"} icon="dots-horizontal" />}
         />
@@ -55,34 +71,27 @@ export default function AdminNotifications() {
 
 const styles = StyleSheet.create({
   header: {
-    color: "white",
     marginBottom: 5,
     paddingLeft: 15,
-    fontSize: 18,
+    fontSize: 24,
     fontFamily: "Montserrat_600SemiBold",
   },
   header2: {
-    color: "white",
     fontSize: 14.5,
-    fontFamily: "Montserrat_700Bold",
+    fontFamily: "Montserrat_600SemiBold",
   },
   container: {
     paddingTop: 90,
-
-    backgroundColor: "black",
     height: "100%",
   },
   listItem: {
-    //  backgroundColor: "#323232",
-    borderBottomColor: "#323232",
     borderBottomWidth: 1,
     borderRadius: 5,
-    padding: 15,
-    marginBottom: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
   },
   iconLeft: {
-    backgroundColor: "#F5F5F9",
     borderRadius: 50,
-    padding: 15,
+    padding: 10,
   },
 });
